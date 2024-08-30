3e3bfa5e401870f94a1af91f4beaeac8a53b6fdd
Mohammad Partovi <85834465+mopa630981@users.noreply.github.com>
2024-08-30 08:39:42 +0330

    Add files via upload

---
+++ mean_var_std.py
@@ -0,0 +1,13 @@
+import numpy as np
+def calculate(input_list):
+    my_mat=np.array(input_list).reshape(3,3)
+    output={
+        'mean':[np.mean(my_mat,axis=0).tolist(),np.mean(my_mat,axis=1).tolist(),np.mean(my_mat).tolist()],
+        'variance':[np.var(my_mat,axis=0).tolist(),np.var(my_mat,axis=1).tolist(),np.var(my_mat).tolist()],
+        'standard deviation':[np.std(my_mat,axis=0).tolist(),np.std(my_mat,axis=1).tolist(),np.std(my_mat).tolist()],
+        'max':[np.max(my_mat,axis=0).tolist(),np.max(my_mat,axis=1).tolist(),np.max(my_mat).tolist()],
+        'min':[np.min(my_mat,axis=0).tolist(),np.min(my_mat,axis=1).tolist(),np.min(my_mat).tolist()],
+        'sum':[np.sum(my_mat,axis=0).tolist(),np.sum(my_mat,axis=1).tolist(),np.sum(my_mat).tolist()]}
+    return output
+
+print(calculate([0,1,2,3,4,5,6,7,8]))
